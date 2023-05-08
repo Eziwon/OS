2a7bc7784141917b5118d5a924d08c6585aebe7b
JiwonLeeeeeeee <eziwon0228@gmail.com>
2023-05-09 00:20:40 +0900

    Update hw5_2_server.c

--- hw5_2_server.c
+++ hw5_2_server.c
@@ -1,3 +1,41 @@
+/*
+    socket_server.c: open a port and converts digit strings into the corresponding alphabet string
+        communicates with the client through SOCKET
+        e.g., "135246" --> "one three five two four six"
+
+        try to make the output as similar to the following example as possible
+
+    Example) ./socket_server 1234        // server-port = 1234
+        port = 1234
+        creating socket...
+        Done.
+        socket binding...
+        Done.
+        listening socket...
+        Done.
+        Waiting for connect request...
+        Connected.
+        input = [1234]                    // received "1234" from the client
+        output = [one two three four]
+        Sending message...                // sending "one two three four" to the client
+        Done.
+        input = [872]
+        output = [eight seven two]
+        Sending message...
+        Done.
+        input = [1004]
+        output = [one zero zero four]
+        Sending message...
+        Done.
+        input = [1829401]
+        output = [one eight two nine four zero one]
+        Sending message...
+        Done.
+        Closing sockets                    // received "quit"
+        Done.
+    
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +46,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+// converts a digit string into the corresponding alphabet string
 void Digit2Text(char *digit_string, char *alphabet_str);
 
 int main(int argc, char *argv[])
@@ -23,6 +62,8 @@ int main(int argc, char *argv[])
 
 	printf("creating socket...\n");
 
+    // TO DO: create a socket (server socket) using the port number. store the socket in an integer variable
+    //    on failure, display an error message and terminate
 	if ((ret = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		perror("failed to create a socket\n");
 		exit(1);
@@ -31,12 +72,15 @@ int main(int argc, char *argv[])
 	printf("Done.\n");
 
 	struct sockaddr_in svr_addr = { 0 };
+    // TO DO: fill svr_addr with appropriate values
 	svr_addr.sin_family = AF_INET;
 	svr_addr.sin_port = htons(port);
 	svr_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	printf("socket binding...\n");
 
+    // TO DO: bind the socket to svr_addr
+    //    on failure, display an error message and terminate
 	if (bind(ret, (struct sockaddr *)&svr_addr, sizeof(svr_addr)) == -1) {
 		perror("bind() error!\n");
 		exit(1);
@@ -46,6 +90,8 @@ int main(int argc, char *argv[])
 
 	printf("listening socket...\n");
 
+    // TO DO: listen the server socket
+    //    on failure, display an error message and terminate
 	if (listen(ret, 5) == 1) {
 		perror("listen() error!\n");
 		exit(1);
@@ -59,6 +105,8 @@ int main(int argc, char *argv[])
 
 	printf("Waiting for connect request...\n");
 
+    // TO DO: accept a connection request by calling acctp(). keep the client socket in an integer variable
+    //    on failure, display an error message and terminate
 	client_addr_len = sizeof(client_addr);
 	int clientSocket = accept(ret, (struct sockaddr *)&client_addr, &client_addr_len);
 	if (clientSocket == -1) {
@@ -68,6 +116,12 @@ int main(int argc, char *argv[])
 
 	printf("Connected.\n");
 
+    /* TO DO: repeat
+        read an input string from the socket using read()
+        if the input is "quit" or an empty string, break
+        convert the input into the corresponding alphabet string by calling Digit2Text()
+        write the alphabet string into the socket using write()
+    */
 	char input[64] = "";
 
 	while(1){
@@ -86,6 +140,7 @@ int main(int argc, char *argv[])
 
 	printf("Closing sockets\n");
 
+    // TO DO: close the two sockets
 	close(clientSocket);
 	close(ret);
 
@@ -101,7 +156,11 @@ void Digit2Text(char *digit_str, char *alphabet_str)
 	static char digit_name[][16] = {
 		"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
 	};
-
+    
+    // TO DO: convert a digit string into the corresponding alphabet string
+    //    e.g., "135246" --> "one three five two four six"
+    //     if digit_string is an empty string or contains a non-digit character
+    //        set alphabet_str to "" and return
 	if (digit_str[0] == 0){
 		strcpy(alphabet_str, "");
 		return;
