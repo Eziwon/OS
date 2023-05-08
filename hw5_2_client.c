ea6f8d2fae3eae35e4661e80d6b868c4661bd382
JiwonLeeeeeeee <eziwon0228@gmail.com>
2023-05-09 00:22:13 +0900

    Update hw5_2_client.c

--- hw5_2_client.c
+++ hw5_2_client.c
@@ -1,3 +1,30 @@
+/*
+    socket_client.c: reads digit strings from the user
+        and converts it into the corresponding alphabet string using socket_server.
+        communicate with socket_client through SOCKET
+        e.g., "135246" --> "one three five two four six"
+
+        try to make the output as similar to the following example as possible
+
+    Example) ./socket_client 127.0.0.1 1234 4321
+        Connecting to 127.0.0.1:1234
+        Done.
+        Input a number: 1234
+        1234 ==> one two three four
+        Input a number: 872
+        872 ==> eight seven two
+        Input a number: 1004
+        1004 ==> one zero zero four
+        Input a number: 1829401
+        1829401 ==> one eight two nine four zero one
+        Input a number: quit
+        quit ==>
+        Closing sockets
+        Done.
+
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,6 +46,8 @@ int main(int argc, char *argv[])
 	char *server_ip = argv[1];
 	int server_port = atoi(argv[2]);
 
+    // TO DO: create a socket
+    //  on failure, display an error message and terminate
 	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == -1){
 		perror("failed to create a socket\n");
@@ -28,17 +57,27 @@ int main(int argc, char *argv[])
 	printf("Connecting to %s:%d\n", server_ip, server_port);
 
 	struct sockaddr_in svr_addr = { 0 };
+    // TO DO: set svr_addr with appropriate values
 	svr_addr.sin_family = AF_INET;
 	svr_addr.sin_port = htons(server_port);
 	svr_addr.sin_addr.s_addr = inet_addr(server_ip);
 
+    // TO DO: connect to svr_addr using the socket
+    //  on failure, display an error message and terminate
 	if (connect(clientSocket, (struct sockaddr *)&svr_addr, sizeof(svr_addr)) == -1){
 		perror("connect() error!\n");
 		exit(1);
 	}
 
 	printf("Done.\n");
-
+    
+    /* TO DO: repeat
+        read a string from the user
+        write the input string to the socket using write()
+        read the conversion result from the socket using read()
+        display the input string and the conversion result
+        if the conversion result is an empty string, break
+    */
 	char input[64] = "";
 	
 	while(1){
@@ -56,6 +95,7 @@ int main(int argc, char *argv[])
 
 	printf("Closing socket\n");
 
+    // TO DO: close the socket
 	close(clientSocket);
 
 	printf("Done.\n");
